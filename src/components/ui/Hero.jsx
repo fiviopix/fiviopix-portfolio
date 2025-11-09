34d7e6236fcebafad305396f3a5a0b1040fb42db
keval-vachhani <kevalvachhani3008@gmail.com>
2025-11-09 15:27:47 +0530

    hero componenet 2nd commit

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='font-bold text-9xl font-gray-900'> This is Hero componenet</div>
+    <div className='font-bold text-9xl font-gray-900'> This is Hero componenet Of Home Page</div>
   )
 }
 
